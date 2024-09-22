918cf75a32f9e0d8aebcfd8c6ebd8177256a73a1
Abhyudaya <48117716+AbhyudayaP@users.noreply.github.com>
2024-09-22 21:22:14 +0530

    added cart related scenarios

--- features/mainPage.feature
+++ features/mainPage.feature
@@ -1,9 +1,12 @@
 @mainPage
-Feature: Business rules 1
+Feature: Main page feaure
 
   Scenario: On Main page user add product to the cart
     Given I am on the HomePage
     Then The user enters name and password
     Then The user clicks on Login button
     When User on the main page
+    Then User clicks on add to cart button
+    Then User clicks on cart icon
+    Then user verifies the item details
  
--- pages/mainPage.js
+++ pages/mainPage.js
@@ -3,6 +3,7 @@ const { I } = inject();
   module.exports= {
     logo: "//div[@class='header_label']/div[@class='app_logo']",
     addToCartButton: "//button[@id='add-to-cart-sauce-labs-backpack']",
+    cartButton: "//a[@class='shopping_cart_link']",
 
     async verifyMainPage(){
         await I.seeElementInDOM(this.logo);
@@ -11,6 +12,14 @@ const { I } = inject();
         I.seeTextEquals('Swag Labs',this.logo);
 
 
+    },
+
+    async clickOnAddtoCartButton(){
+      await I.click(this.addToCartButton);
+    },
+
+    async clickOnCart(){
+      await I.click(this.cartButton);
     }
 
 
